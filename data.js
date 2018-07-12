7091899f536dcd79f67bcfd2a587b14df6c5be81
Rifki Fauzi <versitani@gmail.com>
2018-07-12 17:15:38 +0700

    update

--- data.js
+++ data.js
@@ -107,7 +107,7 @@ function getAndSetClassesFromData(data, numclasses, method) {
   alert("error: Tentukan Metode Klasifikasi!.")
   }
  // var colors_x = chroma.scale([colorFrom, colorTo]).colors(numclasses)
- var colors_x = chroma.scale(BuPu).colors(numclasses)
+ var colors_x = chroma.scale('BuPu').colors(numclasses);
 
 serie.setColors(colors_x);
 document.getElementById('legend').innerHTML = serie.getHtmlLegend(null, "Penduduk KalTeng (Jiwa)</br> Metode:" +methodLabel, 1);
